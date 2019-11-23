e03921b7dedd14bd39b556c44de8341e3ff3bd88
EmirVelazquez <emirleovelazquez@gmail.com>
2019-11-23 13:41:39 -0600

    changed port for express

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
